d4ef63c8bc141dc6abbdc4311e8f94ea6e78af31
Shubham Hadake <hadakesr714@gmail.com>
2022-11-10 00:50:03 +0530

    added ga events for testing

--- src/App.js
+++ src/App.js
@@ -10,12 +10,21 @@ function App() {
     ReactGA.initialize(TRACKING_ID);
     ReactGA.pageview('/home');
   });
+
+  const onTextClick = () => {
+    console.log('onTextClick');
+    ReactGA.event({
+      category: 'Button',
+      action: 'Button clicked',
+      label: 'Test',
+    });
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload updated.
+        <p onClick={onTextClick} style={{ cursor: 'pointer' }}>
+          Edit <code>src/App.js</code> and save to reload updated again.
         </p>
         <a
           className="App-link"
