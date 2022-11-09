7d07bcb25dc36cfa31db3ddeba650bf1a8f265f7
Shubham Hadake <hadakesr714@gmail.com>
2022-11-10 00:37:42 +0530

    added ga config

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactGA from 'react-ga';
+
+const TRACKING_ID = 'UA-249112502-1';
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize(TRACKING_ID);
+    ReactGA.pageview('/home');
+  });
   return (
     <div className="App">
       <header className="App-header">
